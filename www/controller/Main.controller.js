242e96104ba356ff67fce672bb8dd628a4ad2159
Natalia Laborde <natalia.c.laborde@accenture.com>
2017-10-27 16:18:38 -0300

    Added footer with order sorter by name or status.

--- www/controller/Main.controller.js
+++ www/controller/Main.controller.js
@@ -4,6 +4,33 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("SAPUI5ExampleSAPUI5Example.controller.Main", {
+		
+		
+		
+		handleGroup : function (evt) {
+
+			//  sorters
+			var sorters = [];
+			var item = evt.getParameter("selectedItem");
+			var key = (item) ? item.getKey() : null;
+			var list = this.getView().byId("table");
+			var oBinding = list.getBinding("items");
+			
+			if(key =="NameKey"){
+				sorters.push(new sap.ui.model.Sorter("name", false, oBinding));
+				oBinding.sort(sorters);
+			}
+			else if(key =="StatusKey"){
+				sorters.push(new sap.ui.model.Sorter("status", true, oBinding));
+				oBinding.sort(sorters);
+			}else {
+				oBinding.sort();
+			}
+		
+				
+			
+		}
+	
 
 	});
 });
