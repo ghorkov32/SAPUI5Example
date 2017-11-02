c3fb6f401ee26c71cab5ba56c7e80c9b61fee3f8
Natalia Laborde <natalia.c.laborde@accenture.com>
2017-11-02 09:17:59 -0300

    Added folder utility, aggregated formatter, added style in css for row color.

--- www/util/formatter.js
+++ www/util/formatter.js
@@ -1,12 +1,38 @@
-sap.ui.define([], function () {
-	"use strict";
+jQuery.sap.declare("SAPUI5ExampleSAPUI5Example.util.Formatter");
+jQuery.sap.require("sap.ui.core.format.DateFormat");
 
-	return {
 
-		statusState: function (sStatus) {
+SAPUI5ExampleSAPUI5Example.util.Formatter = {
+
+ 
+		statusColorRow: function(oStatus) {
 			
+			
+			
+			switch (oStatus){
+			
+			case "On Duty":
+				return "rowColorOnDuty";
+				
+				break;
+			
+			case "Off work":
+				return "rowColorOffwork"
+				
+				break;
+				
+			case "Leave of absence":
+				return "rowColorLeaveofabsence"
+				
+				break;
+				
+			default:
+				break;
 			
 			}
-		};
-	
-});
+             
+				
+			}
+
+};
+
