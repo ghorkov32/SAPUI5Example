482ec2e7f82d8783ed541d27504dd4e98cd54b54
Germán Piccioni <german.n.piccioni@accenture.com>
2017-11-09 16:57:36 -0300

    * Created a new view for a list of the plants and its corresponding controller. * Added a new property on i18n for the title of that view.

---
+++ www/controller/Plants.controller.js
@@ -0,0 +1,50 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("SAPUI5ExampleSAPUI5Example.controller.Main", {
+
+		handleGroup : function (oEvt) {
+
+			//  sorters
+			var sorters = [];
+			var item = oEvt.getParameter("selectedItem");
+			var key = (item) ? item.getKey() : null;
+			var list = this.getView().byId("table");
+			var oBinding = list.getBinding("items");
+			
+			switch (key){
+			
+			case "NameKey":
+				sorters.push(new sap.ui.model.Sorter("name", false, oBinding));
+				oBinding.sort(sorters);
+				break;
+			
+			case "StatusKey":
+				sorters.push(new sap.ui.model.Sorter("status", true, oBinding));
+				oBinding.sort(sorters);
+				break;
+				
+			case "NoneKey":
+				oBinding.sort();
+				break;
+				
+			default:
+				break;
+			}
+		},
+
+		mailBtnClick: function(oEvt){
+			var sEmail = oEvt.getSource().getCustomData()[0].getValue();
+			sap.m.URLHelper.triggerEmail(sEmail, "Subject", "Hi"); 
+		},
+
+		phoneBtnClick: function(oEvt){
+			var sPhone = oEvt.getSource().getCustomData()[0].getValue();
+			sap.m.URLHelper.triggerTel(sPhone); 
+		}
+		
+		
+	});
+});
