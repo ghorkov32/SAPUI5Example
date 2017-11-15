60bef3d5ea840d83ba03b7a05b2936d6437b3e9c
Natalia Laborde <natalia.c.laborde@accenture.com>
2017-11-15 09:40:09 -0300

    *Added navigation function * Added Mail view Id *Added onNavBack function in Plants page

--- www/controller/Plants.controller.js
+++ www/controller/Plants.controller.js
@@ -1,9 +1,25 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/routing/History"
+], function(Controller, History) {
 	"use strict";
 
 	return Controller.extend("SAPUI5ExampleSAPUI5Example.controller.Plants", {
+		
+		onNavBack: function () {
+			var oHistory, sPreviousHash;
+
+			oHistory = History.getInstance();
+			sPreviousHash = oHistory.getPreviousHash();
+
+			if (sPreviousHash) { 
+				window.history.go(-1);
+			} else {
+				var onRootPage = sap.ui.core.UIComponent.getRouterFor(this);
+				onRootPage.navTo("Main");
+			}
+		}
+		
 
 			
 		
